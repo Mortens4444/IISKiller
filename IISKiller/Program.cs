f4b355e0bf7d0a725160baeceaef48dd585f8cd9
Mortens4444 <mortens.4444@gmail.com>
2019-04-21 23:27:50 +0200

    Initial commit

---
+++ IISKiller/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace IISKiller
+{
+    class Program
+    {
+        const string IIS_EXPRESS = "iisexpress";
+
+        static void Main(string[] args)
+        {
+            bool shouldKill = false;
+
+            while (true)
+            {
+                shouldKill |= IsInternetExplorerRunning() && IsProcessRunning(IIS_EXPRESS);
+
+                if (shouldKill && !IsInternetExplorerRunning())
+                {
+                    KillProcess(IIS_EXPRESS);
+                    shouldKill = false;
+                }
+
+                Thread.Sleep(3000);
+            }
+        }
+
+        static bool IsInternetExplorerRunning()
+        {
+            return IsProcessRunning("iexplore");
+        }
+
+        static bool IsBrowserRunning()
+        {
+            return IsProcessRunning("chrome", "iexplore", "firefox", "opera");
+        }
+
+        static bool IsProcessRunning(params string[] processNames)
+        {
+            foreach (var processName in processNames)
+            {
+                var processes = Process.GetProcessesByName(processName);
+                if (processes.Length > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void KillProcess(string processName)
+        {
+            var processes = Process.GetProcessesByName(processName);
+            foreach (var process in processes)
+            {
+                process.Kill();
+                process.Close();
+            }
+        }
+    }
+}
